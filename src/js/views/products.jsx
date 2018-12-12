6685acebf8c2fbd98189b0d9d0eb22c142e0e739
Ecca Urueta <edibles@gmail.com>
2018-12-12 01:07:11 +0000

    Fixed products.jsx filename

---
+++ src/js/views/products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default class Products extends React.component {
+	render() {
+		return (
+			<div>
+				<h1>Product View</h1>
+				<p>This will be the product page.</p>
+			</div>
+		);
+	}
+}
