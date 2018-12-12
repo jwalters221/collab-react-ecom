3e9ebd00331a8b3e5bf904026a642d1a9984ddd6
Irvine Springer <irvine.springer@gmail.com>
2018-12-12 01:52:10 +0000

    Update to footer, navbar and signin component

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.jsx";
 
 export class Footer extends Component {
 	render() {
-		return <h2 className="text-center">Footer</h2>;
+		return <h6 className="text-center">© 2017-2018</h6>;
 	}
 }
---
+++ src/js/component/signin.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.jsx";
+
+
+export class SignIn extends Component {
+	render() {
+		return (
+			
+			<div className="container mb-5 ">
+			<div className="row d-flex justify-content-center">
+			<div><img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/></div>
+			<div><h2>Please sign in </h2></div>
+			<form>
+			<div className="form-group">
+									
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Email address"
+									/>
+								</div>
+								<div className="form-group">
+								
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Password"
+									/>
+								</div>
+								
+								<div class="form-check">
+          <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked/>
+          <label className="form-check-label" for="gridRadios1">
+            Remember me
+          </label>
+        </div>
+        <button
+									type="submit"
+									className="btn btn-primary mb-2">
+									Sign in
+								</button>
+			</form>
+			
+			</div>
+			</div>
+			
+			
+			);
+	}
+}
