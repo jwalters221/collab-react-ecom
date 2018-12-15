4cad3a09a0eb3c31eaae8c5f32703106fb1df27a
Ecca Urueta <edibles@gmail.com>
2018-12-15 16:05:53 +0000

    Added single Product page with rating component

--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,13 @@ import "../styles/index.css";
 //import your own components
 import Layout from "./layout.jsx";
 
+//FontAwesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+//import {  } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faStar);
+
 //render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
