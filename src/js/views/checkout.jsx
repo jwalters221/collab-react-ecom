b28bc1ee88baec7e83ac33bc20c41afa1b3caa68
Brian E Logsdon <brianlogsdon2@gmail.com>
2018-12-15 16:07:06 +0000

    added modal component for checkout

--- src/js/views/checkout.jsx
+++ src/js/views/checkout.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext.jsx";
+import Modal from "../component/modal.jsx";
 
 export class Checkout extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			showModal: false
+		};
+	}
 	render() {
 		return (
-			<div className="container">
+			<div className="container mt-2">
 				<div className="row">
 					<div className="col-md-4 order-md-2 mb-4">
 						<h4 className="d-flex justify-content-between align-items-center mb-3">
@@ -90,17 +97,31 @@ export class Checkout extends React.Component {
 									placeholder="Enter address"
 								/>
 							</div>
+
 							<h4 className="mb-3">Payment</h4>
 							<div className="form-group">
-								<label>Name on card</label>
-								<input
-									id="card-name"
-									type="text"
-									className="form-control"
-									placeholder="Name on card"
-								/>
+								<div className="row">
+									<div className="col-md-6">
+										<label>First Name</label>
+										<input
+											id="first-name"
+											type="text"
+											className="form-control"
+											placeholder="First Name"
+										/>
+									</div>
+									<div className="col-md-6">
+										<label>Last Name</label>
+										<input
+											id="last-name"
+											type="text"
+											className="form-control"
+											placeholder="Last Name"
+										/>
+									</div>
+								</div>
 							</div>
-							<div className="form-group">
+							<div className=" col-md-6 form-group">
 								<label>Credit card number</label>
 								<input
 									id="cc-number"
@@ -110,17 +131,94 @@ export class Checkout extends React.Component {
 								/>
 							</div>
 							<div className="form-group">
-								<label>Expiration</label>
-								<input
-									id="address"
-									type="text"
-									className="form-control"
-									placeholder="Expiration Date"
-								/>
+								<label
+									className="col-sm-3 control-label"
+									htmlFor="expiry-month">
+									Expiration Date
+								</label>
+								<div className="col-sm-9">
+									<div className="row">
+										<div className="col-xs-3">
+											<select
+												className="form-control"
+												name="expiry-month"
+												id="expiry-month">
+												<option>Month</option>
+												<option value="01">
+													Jan (01)
+												</option>
+												<option value="02">
+													Feb (02)
+												</option>
+												<option value="03">
+													Mar (03)
+												</option>
+												<option value="04">
+													Apr (04)
+												</option>
+												<option value="05">
+													May (05)
+												</option>
+												<option value="06">
+													June (06)
+												</option>
+												<option value="07">
+													July (07)
+												</option>
+												<option value="08">
+													Aug (08)
+												</option>
+												<option value="09">
+													Sep (09)
+												</option>
+												<option value="10">
+													Oct (10)
+												</option>
+												<option value="11">
+													Nov (11)
+												</option>
+												<option value="12">
+													Dec (12)
+												</option>
+											</select>
+										</div>
+										<div className="col-xs-3">
+											<select
+												className="form-control"
+												name="expiry-year">
+												<option value="13">2013</option>
+												<option value="14">2014</option>
+												<option value="15">2015</option>
+												<option value="16">2016</option>
+												<option value="17">2017</option>
+												<option value="18">2018</option>
+												<option value="19">2019</option>
+												<option value="20">2020</option>
+												<option value="21">2021</option>
+												<option value="22">2022</option>
+												<option value="23">2023</option>
+											</select>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div>
+								<button
+									type="button"
+									onClick={() =>
+										this.setState({ showModal: true })
+									}
+									className="btn btn-success">
+									Submit payment
+								</button>
 							</div>
 						</form>
 					</div>
 				</div>
+				<Modal
+					show={this.state.showModal}
+					onClose={() => this.setState({ showModal: false })}
+				/>
 			</div>
 		);
 	}
